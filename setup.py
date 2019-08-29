753906bd0a232e026395a10c0b8b58b2bf5c4b2f
Petar Marić <petar.maric@gmail.com>
2019-08-29 23:20:07 +0200

    Renamed README to README.rst to prevent GitHub from rendering it as plain text

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email='petarmaric@uns.ac.rs',
     description='Console app and Python API for harvesting email addresses of '\
                 'our ACS students',
-    long_description=open('README').read(),
+    long_description=open('README.rst').read(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
